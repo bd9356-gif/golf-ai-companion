7c583d93fd3a2acba23e00660ecaaba42be9376f
bd9356-gif <bd9356@gmail.com>
2026-03-29 09:23:14 -0400

    Shorten Update My Plan to fit mobile nav

--- app/plan/page.js
+++ app/plan/page.js
@@ -175,7 +175,7 @@ export default function MyPlanPage() {
                 href="/onboarding"
                 className="text-sm font-semibold text-green-700 border-2 border-green-700 rounded-xl px-4 py-2 hover:bg-green-50 transition-colors whitespace-nowrap"
               >
-                Update My Plan
+                Update Plan
               </a>
             </div>
           </div>
