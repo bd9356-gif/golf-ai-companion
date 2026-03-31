052b0162702752a9fd3b9d968568e19f3e3f59c7
bd9356-gif <bd9356@gmail.com>
2026-03-31 13:55:36 -0400

    Button label MyLevel one word

--- app/plan/page.js
+++ app/plan/page.js
@@ -191,7 +191,7 @@ export default function MyPlanPage() {
             </button>
             <div className="ml-auto">
               <a href="/onboarding" className="text-sm font-semibold text-green-700 border-2 border-green-700 rounded-xl px-4 py-2 hover:bg-green-50 transition-colors whitespace-nowrap">
-                My Level
+                MyLevel
               </a>
             </div>
           </div>
