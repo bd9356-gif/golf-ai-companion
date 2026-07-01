06ec12d68d5c24c07c6c3098bf8163da39b925ae
Bill D <bd9356@gmail.com>
2026-07-01 12:30:34 -0400

    Bigger action buttons

--- MyGolf Companion/MyGolf Companion/Ihadafiveview.swift
+++ MyGolf Companion/MyGolf Companion/Ihadafiveview.swift
@@ -710,10 +710,4 @@ struct MonthSection: View {
 }
 
 // MARK: - Share Sheet
-struct ShareSheet: UIViewControllerRepresentable {
-    let items: [Any]
-    func makeUIViewController(context: Context) -> UIActivityViewController {
-        UIActivityViewController(activityItems: items, applicationActivities: nil)
-    }
-    func updateUIViewController(_ uvc: UIActivityViewController, context: Context) {}
-}
+
